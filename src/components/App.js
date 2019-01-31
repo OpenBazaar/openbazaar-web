c836ee0ed72ab26235b4de6f12041434ef6fb3e0
Rob Misiorowski <rmisio@yahoo.com>
2019-01-31 16:24:17 -0600

    wip - settings button on main nav bar

--- src/components/App.js
+++ src/components/App.js
@@ -5,55 +5,34 @@ import * as ModalActions from 'actions/modals';
 import { Route } from 'react-router';
 import { Link } from 'react-router-dom';
 import ModalRoot from 'components/modals/ModalRoot';
-import SimpleMessage from 'components/modals/SimpleMessage';
-import 'App.scss';
+import Discovery from 'components/pages/Discovery';
+import './App.scss';
 import 'styles/layout.scss';
+import 'styles/ui/buttons.scss';
+import logo from 'img/ob-logo.png';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.speakInputRef = React.createRef();
-    this.handleFartClick = this.handleFartClick.bind(this);
-  }
-
-  handleFartClick() {
-    this.props.actions.modals.open({
-      Component: SimpleMessage,
-      title: 'You smell',
-      body: "You smell like hell Jerry! Why don't you work on that player!?"
-    });
-  }
   render() {
-    const pathname =
-      this.props.router.location && this.props.router.location.pathname;
-
     return (
       <div className="App">
         <header className="App-header">
           <nav className="flexVCent row gutterH">
-            <Link to="/" className={pathname === '/' ? 'active' : ''}>
-              Home
+            <Link to="/" className="App-logo-wrap">
+              <img src={logo} className="App-logo" alt="logo" />
             </Link>
-            <Link to="/about" className={pathname === '/about' ? 'active' : ''}>
-              About
-            </Link>
+            <div className="flexHRight">
+              <div className="flexExpand">
+                <button className="App-btnSettings iconBtn"></button>
+              </div>
+            </div>
           </nav>
         </header>
         <div className="App-mainContent">
-          <div className="row flexCent gutterH">
-            <input type="text" ref={this.speakInputRef} />
-            <button onClick={this.handleSpeakClick}>Speak it punk</button>
-            <button onClick={this.handleFartClick}>Fart it out</button>
-          </div>
           <div>
             <Route
               exact
               path="/"
-              component={() => <div>say you. say me.</div>}
-            />
-            <Route
-              path="/profile"
-              component={() => <div>Charlie chuckles.</div>}
+              component={Discovery}
             />
           </div>
         </div>
