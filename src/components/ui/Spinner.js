c90e0758308d95ba0bf3661721815226c4301502
Rob Misiorowski <rmisio@yahoo.com>
2019-02-11 16:04:54 -0600

    implemented the catgory box fetching and fetchError states

--- src/components/ui/Spinner.js
+++ src/components/ui/Spinner.js
@@ -10,11 +10,11 @@ const sizes = {
 
 const Spinner = props => {
   const width = props.width || sizes[props.size];
-  const height = props.height || sizes[props.height];
+  const height = props.height || sizes[props.size];
 
   return (
-    <svg version="1.1" class="spinner" style={{ width, height }} xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-     width="20px" height="40px" viewBox="0 0 40 40" enable-background="new 0 0 20 40" xmlSpace="preserve">
+    <svg version="1.1" className="spinner" style={{ width, height }} xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+     width="20px" height="40px" viewBox="0 0 40 40" enableBackground="new 0 0 20 40" xmlSpace="preserve">
     <path opacity="0.2" fill="#000" d="M20.201,5.169c-8.254,0-14.946,6.692-14.946,14.946c0,8.255,6.692,14.946,14.946,14.946
       s14.946-6.691,14.946-14.946C35.146,11.861,28.455,5.169,20.201,5.169z M20.201,31.749c-6.425,0-11.634-5.208-11.634-11.634
       c0-6.425,5.209-11.634,11.634-11.634c6.425,0,11.633,5.209,11.633,11.634C31.834,26.541,26.626,31.749,20.201,31.749z"/>
@@ -40,10 +40,18 @@ Spinner.defaultProps = {
 
 Spinner.propTypes = {
   size: PropTypes.oneOf(Object.keys(sizes)),
-  width: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.null,
-  ]),
+  width: function(props, propName, componentName) {
+    if (props.width !== null &&
+      (typeof props.width !== 'string' || !props.width)) {
+      throw new Error('Width must be null or a non-empty string.')
+    }
+  },
+  height: function(props, propName, componentName) {
+    if (props.height !== null &&
+      (typeof props.height !== 'string' || !props.height)) {
+      throw new Error('Height must be null or a non-empty string.')
+    }
+  },
 }
 
 export default Spinner;
