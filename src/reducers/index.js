a74d28577cea696d23e412b3df2a629218911f01
Rob Misiorowski <rmisio@users.noreply.github.com>
2019-02-20 14:05:31 -0600

    ran prettier

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,21 +2,14 @@ import { connectRouter } from 'connected-react-router';
 import modals from './modals';
 import responsive from './responsive';
 import discovery from './discovery';
-<<<<<<< HEAD
 import auth from './auth';
 import navMenu from './navMenu';
-=======
->>>>>>> initial-skeleton
 
 export default history => ({
   router: connectRouter(history),
   modals,
   responsive,
-<<<<<<< HEAD
   discovery,
   auth,
   navMenu
-=======
-  discovery
->>>>>>> initial-skeleton
 });
